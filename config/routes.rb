1a4356be90edaf1d17bf91726251f88f8e5f9610
BenjaminRasmussen <dumstick228@gmail.com>
2018-08-29 18:51:31 +0200

    added Basic user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help', as: 'help'
   get '/about',   to: 'static_pages#about', as: 'about'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   get '/signup', to: 'users#new'
-  
+
+  get 'users/new'
+
 end
